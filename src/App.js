2291b0f0bb7d1e432a5c82fd0ba0ecbddc12597c
guk1961 <guk1961@gmail.com>
2023-03-09 17:08:36 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
-// import Header from "./components/Header/Header";
+import Header from "./components/Header/Header";
 // import {Route, Routes} from 'react-router-dom'
 
 // const tg = window.Telegram.WebApp
@@ -11,10 +11,11 @@ function App() {
     //
     useEffect(() => {
         tg.ready();
-    }, [])
+    })
 
     return (
         <div className="App">
+            <Header/>
             <button onClick={onToggleButton}>toggle</button>
         </div>
     );
