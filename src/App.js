33d81eb7872e10645debd47bbe69a8652b83af45
guk1961 <guk1961@gmail.com>
2023-03-09 16:29:36 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
 import React, {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
-import Header from "./components/Header/Header";
-import {Route, Routes} from 'react-router-dom'
+//import {useTelegram} from "./hooks/useTelegram";
+// import Header from "./components/Header/Header";
+// import {Route, Routes} from 'react-router-dom'
+
+const tg = window.Telegram.WebApp
 
 function App() {
-    const {onToggleButton, tg} = useTelegram();
-
+    // const {onToggleButton, tg} = useTelegram();
+    //
     useEffect(() => {
         tg.ready();
     }, [])
+    const onClose = () => {
+        tg.close()
+    }
 
     return (
         <div className="App">
-            <Header />
-            <Routes>
-            </Routes>
+            work
+            <button onClick={onClose}>Закрыть</button>
         </div>
     );
 }
