89255330f83bdb8658617f745c0ffd2d12502318
guk1961 <guk1961@gmail.com>
2023-03-09 20:21:49 +0300

    4

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://85.119.146.179:8000/web-data', {
+        fetch('http://185.252.147.123:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
